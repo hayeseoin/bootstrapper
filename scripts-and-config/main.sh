527e4a0b1f83b84a0a873c601a4e4e0bd80eeffa
Eoin Hayes <eoahayes13@gmail.com>
2025-06-29 08:37:44 +0200

    first pass

--- scripts-and-config/main.sh
+++ scripts-and-config/main.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
 
+DOTFILES_REPO="git@github.com:hayeseoin/dotfiles.git"
+DOTFILES_REPO_NAME="dotfiles"
+
+UTILS_AND_SCRIPTS_REPO="git@github.com:hayeseoin/utils-and-scripts.git"
+UTILS_AND_SCRIPTS_REPO_NAME="utils-and-scripts"
+UTILS_AND_SCRIPTS_MODE="" # system or local
+
+# Validate git connection
+# Expected output code is 1 - i.e. a clean fail means we can connect
+ssh -T -o StrictHostKeyChecking=no git@github.com > /dev/null 2>&1
+if [[ $? -ne 1 ]]; then
+    echo "Can't connect to git, ensure SSH keys are set"
+    exit 1
+fi
+echo "Connected to git."
+
+
+# Dotfiles
+echo 'Checking dotfiles directory exists'
+if [[ ! -d $HOME/$DOTFILES_REPO_NAME ]]; then
+    echo "Dotfiles directory doesn't exist, creating it."
+    git clone $DOTFILES_REPO $HOME/$DOTFILES_REPO_NAME
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to clone repository"
+        exit 1
+    fi
+fi
+echo 'Dotfiles directory exists.'
+
+if [[ ! -d $HOME/$DOTFILES_REPO_NAME/.git ]]; then
+    echo 'Dotfiles exists but is not a git repo, exiting...'
+    exit 1
+fi
+echo "Dotfiles directory is a git repo."
+
+# Dotfiles setup script
+$HOME/$DOTFILES/setup/main.sh
